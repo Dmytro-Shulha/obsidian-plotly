d9493aeaf4c5b09cc8ae75f9933e702de012050c
Dmitriy Shulha <dimashulga1115@gmail.com>
2021-10-05 21:04:53 +0300

    Reformat imports, update basic plot template.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, Menu, Editor, MarkdownView } from 'obsidian';
-import { addIcons, PLOTLY_LOGO } from "./ui/icons";
-import { preprocessor } from './preprocessor';
+import './show-hint'
+
+import { App, Editor, MarkdownView, Menu, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { PLOTLY_LOGO, addIcons } from "./ui/icons";
+
 import PlotlyModal from './ui/modal';
 import PlotlyPluginSettingTab from './ui/settingsTab'
-import './show-hint'
+import { preprocessor } from './preprocessor';
+
+const basicPlotlyChart = [
+	'```plotly',
+	'data:',
+	'\t- x: [0,1,2]',
+	'\t  y: [0,1,0]',
+	'```\n'
+].join('\n')
 
 const NEW_PLOTLY_CHART_NAME = "New Plotly Chart";
 const newPlotlyChart = (editor: Editor)=>{
 	let doc = editor.getDoc();
 	let cursor = doc.getCursor();
-	doc.replaceRange("```plotly\ndata:\n\t- x: [0,1,2]\n\t  y: [0,1,0]\n```\n", cursor);
+	doc.replaceRange(basicPlotlyChart, cursor);
 }
 
 export default class PlotlyPlugin extends Plugin {
